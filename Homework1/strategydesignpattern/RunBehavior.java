e99c6ae8e51a1901deadf43a5f6f48a236624a7f
Oliver Mills <mills.oliver@ymail.com>
2021-01-12 22:57:19 -0500

    another change

--- Homework1/strategydesignpattern/RunBehavior.java
+++ Homework1/strategydesignpattern/RunBehavior.java
@@ -1,8 +1,37 @@
 package strategydesignpattern;
 
+import java.util.Random;
+
 public class RunBehavior implements OffenceBehavior {
     public String play()
     {
-        //TODO return random shit
+        String ret = "";
+        int numChoices = 4;
+        Random rand = new Random();
+        int choice = rand.nextInt(numChoices);
+
+        if(choice == 0)
+        {
+            ret = "drive (up the gut)";
+        }
+        else if(choice == 1)
+        {
+            ret = "draw";
+        }
+        else if(choice == 2)
+        {
+            ret = "pitch";
+        }
+        else if(choice == 3)
+        {
+            ret = "reverse";
+        }
+        else
+        {
+            // There is an error. This should not happen.
+            ret = "error";
+        }
+
+        return "runs a " + ret;
     }
 }
